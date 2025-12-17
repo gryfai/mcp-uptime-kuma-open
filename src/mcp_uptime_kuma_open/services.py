711d53f9bd4cfaed5280cb8137642af46828cfc9
cfossto <70951673+cfossto@users.noreply.github.com>
2025-12-17 22:56:39 +0100

    Heartbeats added and tested. Added an env example file as well for end users to use.

--- src/mcp_uptime_kuma_open/services.py
+++ src/mcp_uptime_kuma_open/services.py
@@ -1,6 +1,7 @@
 from uk_client import KumaService
 from os import getenv
 from dotenv import load_dotenv
+from fastmcp.exceptions import ToolError
 
 load_dotenv()
 
@@ -10,16 +11,18 @@ UPTIME_KUMA_PASSWORD=getenv("UPTIME_KUMA_PASSWORD", None)
 
 kuma_service = KumaService(UPTIME_KUMA_URL,UPTIME_KUMA_USERNAME, UPTIME_KUMA_PASSWORD)
 
-def get_all_monitors() -> list[dict]:
+async def get_all_monitors() -> list[dict]:
     """
     Gets a list of Uptime Kuma Monitors.
     :return: List of Uptime Kuma Monitors
     """
-    with kuma_service as api:
-        return api.get_monitors()
+    try:
+        with kuma_service as api:
+            return api.get_monitors()
+    except Exception as e:
+            raise ToolError(e)
 
-
-def get_specific_monitor(id_: int) -> dict:
+async def get_specific_monitor(id_: int) -> dict:
     """
     Gets a specific Uptime Kuma Monitor by Id. Id needs to be integer.
     Use the Id (integer) to interact with a specific monitor. Whenever the user
@@ -27,6 +30,35 @@ def get_specific_monitor(id_: int) -> dict:
     :param id_: Integer Id of monitor
     :return: dictionary of monitor
     """
-    with kuma_service as api:
-        return api.get_monitor(id_)
+    try:
+        with kuma_service as api:
+            return api.get_monitor(id_)
+    except Exception as e:
+            raise ToolError(e)
 
+def get_specific_monitor_beats(
+        id_: int, hours: int
+) -> list[dict]:
+    """
+    Gets a specific monitor beat per ID specified as an integer.
+    :param id_: Integer ID of the monitor
+    :param hours: Time period from now
+    :return: Monitor beats as list of dictionaries.
+    """
+    try:
+        with kuma_service as api:
+            return api.get_monitor_beats(id, hours)
+    except Exception as e:
+        raise ToolError(e)
+
+
+def get_all_heartbeats() -> dict:
+    """
+    Gets all heartbeats available for all monitors.
+    :return: Dictionary with all heartbeats
+    """
+    try:
+        with kuma_service as api:
+            return api.get_heartbeats()
+    except Exception as e:
+        raise ToolError(e)
--- src/mcp_uptime_kuma_open/tools.py
+++ src/mcp_uptime_kuma_open/tools.py
@@ -1,14 +1,16 @@
 from fastmcp import FastMCP
 from services import (
     get_all_monitors,
-    get_specific_monitor
+    get_specific_monitor,
+    get_specific_monitor_beats,
+    get_all_heartbeats
 )
 
 
 def register_tools(free_mcp: FastMCP):
 
     @free_mcp.tool()
-    async def get_uptime_kuma_monitors() -> list[dict]:
+    def get_uptime_kuma_monitors() -> list[dict]:
         """
         Tool is used to get all available monitors for Uptime Kuma. As soon as
         a call is made to handle all monitors, this method will return a list with
@@ -19,7 +21,7 @@ def register_tools(free_mcp: FastMCP):
         return get_all_monitors()
 
     @free_mcp.tool()
-    async def get_specific_uptime_kuma_monitor(id: int) -> dict:
+    def get_specific_uptime_kuma_monitor(id: int) -> dict:
         """
         This tool is used to get a specific Uptime Kuma monitor if the user
         specify a monitor id as an integer. This tool does NOT find a monitor
@@ -28,3 +30,33 @@ def register_tools(free_mcp: FastMCP):
         :return: A dictionary with the specific Uptime Kuma Monitor
         """
         return get_specific_monitor(id)
+
+    @free_mcp.tool()
+    def get_all_monitor_heartbeats() -> dict:
+        """
+        Gets all heartbeats per monitor. The first integer in the response is
+        the monitor id, followed by a list of its heartbeats. This tool is used
+        to get all monitor heartbeats from all monitors. It can be used to get
+        and further filter or do other operations per monitor - non-destructive such!
+
+        :return: Dictionary with all beats per monitor.
+        """
+        return get_all_heartbeats()
+
+
+
+    @free_mcp.tool()
+    def get_monitor_beats_per_id(
+            id: int,
+            hours: int
+    ) -> list[dict]:
+        """
+        This tool is used to get monitor beats per monitor id. Monitor id is defined
+        via an Id and is required to get the beat. It also takes in an "hours" as an integer
+        to specify the time frame in hours from now to the hours:int.
+
+        :param id: Monitor id.
+        :param hours: Hours from now. Counting backwards.
+        :return: Dictionary with Beats
+        """
+        return get_specific_monitor_beats(id, hours)
