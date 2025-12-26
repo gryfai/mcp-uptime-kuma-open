5b902e9f3e5d7bdaf54881264a3bf489d2a4a868
cfossto <70951673+cfossto@users.noreply.github.com>
2025-12-26 23:27:53 +0100

    Updated structure a bit to make it easier to import in the closed version.

--- src/mcp_uptime_kuma_open/server.py
+++ src/mcp_uptime_kuma_open/server.py
@@ -1,10 +1,12 @@
 from fastmcp import FastMCP
 from .tools import register_tools
 
+free_mcp = FastMCP("Free Uptime Kuma MCP server")
+register_tools(free_mcp)
+
 def main():
-    free_mcp = FastMCP("Free Uptime Kuma MCP server")
-    register_tools(free_mcp)
     free_mcp.run()
+
 
 if __name__ == '__main__':
     main()
